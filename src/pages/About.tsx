5f54cb5010af76377680cce88b43703a65982358
ClaudiaAzambuja <cclabdis@gmail.com>
2025-03-06 21:53:44 -0300

    fix about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,17 +1,59 @@
-import { Container, Title, Text, Group, Button } from "@mantine/core";
+import { Container, Title, Text, Group, Button, Box } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    <Container size="lg" style={{ height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center" }}>
-      <Title>Oi, eu sou Claudia Azambuja!</Title>
-      <Text mt="md">Sou desenvolvedora e amo criar soluções incríveis com código! 🚀</Text>
-      
-      <Group mt="xl">
-        <Button component={Link} to="/skills">Habilidades</Button>
-        <Button component={Link} to="/testimonials">Depoimentos</Button>
-        <Button component={Link} to="/contact">Contato</Button>
-      </Group>
-    </Container>
+    <Box
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        backgroundImage: "url('https://vetex.vet.br/blog/wp-content/uploads/2021/12/Dachshund-o-cao-salsicha-930x620.png)", 
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Container
+        size="lg"
+        style={{
+          paddingTop: "20px",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.5)", 
+          borderRadius: "8px",
+        }}
+      >
+        <Title order={2} style={{ color: "white" }}>
+          cclabdis
+        </Title>
+        <Group >
+          <Button variant="link" component={Link} to="/skills" style={{ color: "white" }}>
+            Habilidades
+          </Button>
+          <Button variant="link" component={Link} to="/testimonials" style={{ color: "white" }}>
+            Depoimentos
+          </Button>
+          <Button variant="link" component={Link} to="/contact" style={{ color: "white" }}>
+            Contato
+          </Button>
+        </Group>
+      </Container>
+
+  
+      <Container
+        size="lg"
+        style={{
+          textAlign: "center",
+          backgroundColor: "rgba(255, 255, 255, 0.8)", 
+          borderRadius: "8px",
+          padding: "20px",
+        }}
+      >
+        <Title order={1}>Oi, eu sou Claudia Azambuja!</Title>
+        <Text mt="md">Sou desenvolvedora e amo criar soluções incríveis com código! 🚀</Text>
+      </Container>
+    </Box>
   );
 }
